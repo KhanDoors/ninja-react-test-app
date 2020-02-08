669de169c11d167f773dd6de6975e29c0b70c9bb
syyad <syyadkhan@yahoo.com>
2020-02-08 09:51:51 -0500

    conext done

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
 
 const BookForm = () => {
+  const { addBooks } = useContext(BookContext);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addBooks(title, author);
+    setAuthor("");
+    setTitle("");
+  };
+
   return (
-    <div>
-      <h1>Book Form</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Book Title"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Book Author"
+        value={author}
+        onChange={e => setAuthor(e.target.value)}
+        required
+      />
+      <input type="submit" value="add book" />
+    </form>
   );
 };
 
